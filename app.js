b903fee115adaf1d3b1d0f94ac2bcffbff2a98be
EWC <bluesky7221@naver.com>
2016-08-06 13:06:47 +0900

    frist

---
+++ app.js
@@ -0,0 +1,30 @@
+//import modules
+var express = require('express');
+var app = express();
+var path = require('path');
+var mongoose = require('mongoose');
+
+//connect database
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once("open", function() {
+  console.log("DB connected !");
+});
+db.on("error", function(err) {
+  console.log("DB error : ");
+});
+
+//model setting
+
+//view setting
+app.set("view engine", 'ejs');
+
+//set middlewares
+app.use(express.static(path.join(_dirname, 'public')));
+
+//set routes
+
+//start server
+app.listen(3000, function() {
+  console.log("server on !");
+});
